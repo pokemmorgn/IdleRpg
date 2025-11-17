8a89c92700c4815457cb2916cb3dc1078e978b7e
pokemmorgn <pokemmorgn@gmail.com>
2025-11-17 19:15:33 +0100

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,6 +14,7 @@ import authRoutes from "./routes/authRoutes";
 import serverRoutes from "./routes/serverRoutes";
 import profileRoutes from "./routes/profileRoutes";
 import invitationRoutes from "./routes/invitationRoutes";
+import gameDataRoutes from "./routes/gameDataRoutes";
 
 dotenv.config();
 
@@ -93,6 +94,7 @@ app.use("/auth", authRoutes);
 app.use("/servers", serverRoutes);
 app.use("/profile", profileRoutes);
 app.use("/invitation", invitationRoutes);
+app.use("/game-data", gameDataRoutes);
 
 
 // -------------------------
