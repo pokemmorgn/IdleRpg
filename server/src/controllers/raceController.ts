92f4d1162c780d3bf34665f7392f1a16dfb0605c
pokemmorgn <pokemmorgn@gmail.com>
2025-11-20 13:16:04 +0100

    Create raceController.ts

---
+++ server/src/controllers/raceController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import { ALL_RACES, getRaceById, getAllowedClassesForRace } from "../config/races.config";
+
+export function getAllRaces(req: Request, res: Response) {
+  return res.json({ races: ALL_RACES });
+}
+
+export function getRace(req: Request, res: Response) {
+  const race = getRaceById(req.params.raceId);
+
+  if (!race) {
+    return res.status(404).json({ error: "Race not found" });
+  }
+
+  return res.json(race);
+}
+
+export function getRaceAllowedClasses(req: Request, res: Response) {
+  const race = getRaceById(req.params.raceId);
+
+  if (!race) {
+    return res.status(404).json({ error: "Race not found" });
+  }
+
+  const classes = getAllowedClassesForRace(race.raceId);
+  return res.json({ race: race.raceId, classes });
+}
