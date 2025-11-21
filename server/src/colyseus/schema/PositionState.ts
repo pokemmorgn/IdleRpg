07ebc4ba8b911a4f4032b6897c8d0ee6484b0895
pokemmorgn <pokemmorgn@gmail.com>
2025-11-21 18:00:51 +0100

    Delete server/src/colyseus/schema/PositionState.ts

--- server/src/colyseus/schema/PositionState.ts
+++
@@ -1,8 +0,0 @@
-import { Schema, type } from "@colyseus/schema";
-
-export class PositionState extends Schema {
-  @type("string") zoneId: string = "default";
-  @type("number") x: number = 0;
-  @type("number") y: number = 0;
-  @type("number") z: number = 0;
-}
