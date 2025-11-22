3beec5e66ed4365018858659567d83f14c0c374c
pokemmorgn <pokemmorgn@gmail.com>
2025-11-22 11:57:49 +0100

    Update InventorySlot.ts

--- server/src/colyseus/schema/InventorySlot.ts
+++ server/src/colyseus/schema/InventorySlot.ts
@@ -1,10 +1,14 @@
 // server/src/colyseus/schema/InventorySlot.ts
-import { Schema, type } from "@colyseus/schema";
+
+import { Schema, type, MapSchema } from "@colyseus/schema";
 
 export class InventorySlot extends Schema {
 
     @type("string") itemId: string = "";
     @type("number") amount: number = 0;
+
+    // 🔥 Ajout : les stats transportées par cet item
+    @type({ map: "number" }) stats = new MapSchema<number>();
 
     constructor() {
         super();
@@ -18,11 +22,17 @@ export class InventorySlot extends Schema {
     clear() {
         this.itemId = "";
         this.amount = 0;
+        this.stats.clear();   // 🔥 nettoyer les stats !
     }
 
     copyFrom(other: InventorySlot) {
         this.itemId = other.itemId;
         this.amount = other.amount;
+
+        this.stats.clear();
+        for (const [k, v] of other.stats.entries()) {
+            this.stats.set(k, v);
+        }
     }
 
     isEmpty() {
