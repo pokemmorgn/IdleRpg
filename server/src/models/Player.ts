6bfc861d77fe6af5be3d2c78b037ec5a27200dad
pokemmorgn <pokemmorgn@gmail.com>
2025-11-23 07:11:29 +0100

    Update Player.ts

--- server/src/models/Player.ts
+++ server/src/models/Player.ts
@@ -4,22 +4,16 @@ export interface IPlayer extends Document {
   username: string;
   email?: string;
   password: string;
-  level: number;
-  xp: number;
-  gold: number;
+
   lastOnline: Date;
 }
 
 const PlayerSchema = new Schema<IPlayer>({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   email: { type: String },
   password: { type: String, required: true },
 
-  level: { type: Number, default: 1 },
-  xp: { type: Number, default: 0 },
-  gold: { type: Number, default: 0 },
-
-  lastOnline: { type: Date, default: Date.now },
+  lastOnline: { type: Date, default: Date.now }
 }, { timestamps: true });
 
 export default mongoose.model<IPlayer>("Player", PlayerSchema);
